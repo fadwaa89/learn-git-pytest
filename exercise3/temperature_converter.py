b6e46fa181b9a0aa96194aac45a84bd61a116033
fadwaa89 <fadwahammouche764@gmail.com>
2025-03-10 20:09:09 +0100

    Implement solutions for exercises

--- exercise3/temperature_converter.py
+++ exercise3/temperature_converter.py
@@ -6,67 +6,19 @@ Temperature = Union[int, float]
 
 
 def celsius_to_fahrenheit(celsius: Temperature) -> float:
-    """
-    Convert temperature from Celsius to Fahrenheit.
-
-    Formula: F = C × 9/5 + 32
-
-    Args:
-        celsius: Temperature in Celsius
-
-    Returns:
-        Temperature in Fahrenheit (rounded to 2 decimal places)
-    """
-    # TODO: Implement this function
-    pass
-
+    return (celsius*9/5)+32
 
 def fahrenheit_to_celsius(fahrenheit: Temperature) -> float:
-    """
-    Convert temperature from Fahrenheit to Celsius.
-
-    Formula: C = (F - 32) × 5/9
-
-    Args:
-        fahrenheit: Temperature in Fahrenheit
-
-    Returns:
-        Temperature in Celsius (rounded to 2 decimal places)
-    """
-    # TODO: Implement this function
-    pass
+        return (fahrenheit - 32) * 5 / 9
 
 
 def celsius_to_kelvin(celsius: Temperature) -> float:
-    """
-    Convert temperature from Celsius to Kelvin.
-
-    Formula: K = C + 273.15
-
-    Args:
-        celsius: Temperature in Celsius
-
-    Returns:
-        Temperature in Kelvin (rounded to 2 decimal places)
-    """
-    # TODO: Implement this function
-    pass
-
+    return (celsius+273.15)
 
 def kelvin_to_celsius(kelvin: Temperature) -> float:
-    """
-    Convert temperature from Kelvin to Celsius.
+        if kelvin < 0:
+            raise ValueError("Temperature cannot be below absolute zero (0K)")
+        return kelvin - 273.15
 
-    Formula: C = K - 273.15
 
-    Args:
-        kelvin: Temperature in Kelvin
 
-    Returns:
-        Temperature in Celsius (rounded to 2 decimal places)
-
-    Raises:
-        ValueError: If kelvin is less than 0 (below absolute zero)
-    """
-    # TODO: Implement this function
-    pass
